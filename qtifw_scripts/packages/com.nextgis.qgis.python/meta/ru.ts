9cab5ccf50244181760582e971751937164862a0
Maxim Dubinin <sim@gis-lab.info>
2017-05-27 16:08:29 +0300

    Update ru.ts

--- qtifw_scripts/packages/com.nextgis.qgis.python/meta/ru.ts
+++ qtifw_scripts/packages/com.nextgis.qgis.python/meta/ru.ts
@@ -4,8 +4,8 @@
 <context>
     <name>script</name>
     <message>
-        <source>QGIS python</source>
-        <translation>QGIS python</translation>
+        <source>QGIS Python</source>
+        <translation>QGIS Python</translation>
     </message>
 </context>
 </TS>
