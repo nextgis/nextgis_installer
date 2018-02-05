5bb1d16f5a45a849549dd75464f4e0d72f442682
Dmitry Baryshnikov <bishop.dev@gmail.com>
2018-02-05 17:36:53 +0300

    Add debug info to build_installer.py

--- qtifw/tools/build_installer.py
+++ qtifw/tools/build_installer.py
@@ -77,7 +77,10 @@ def init():
         qmake = os.path.join(args.qt_dir, 'qmake')
     else:
         # This is buildbot run. Get absolute path.
-        abs_qt_dir = os.path.abspath(os.path.join(src_dir, '/../../../', args.qt_dir))
+        qt_dir = os.path.join(src_dir, '/../../../', args.qt_dir)
+        print 'qt_dir: ' + qt_dir
+        abs_qt_dir = os.path.abspath(qt_dir)
+        print 'abs_qt_dir: ' + abs_qt_dir
         for subdir in os.listdir(abs_qt_dir):
             test_path = os.path.join(abs_qt_dir, subdir, "qtbase")
             if os.isdir(test_path):
