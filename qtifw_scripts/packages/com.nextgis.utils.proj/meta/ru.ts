7584b906598794f1b87cfa9b9d87d5978461becd
Dmitry Baryshnikov <bishop.dev@gmail.com>
2018-02-22 14:07:23 +0300

    Add cs2cs

---
+++ qtifw_scripts/packages/com.nextgis.utils.proj/meta/ru.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>script</name>
+    <message>
+        <source>Proj4 utilities</source>
+        <translation>Утилиты Proj4</translation>
+    </message>
+    <message>
+        <source>Proj4 command line utilities</source>
+        <translation>Утилиты командной строки Proj4</translation>
+    </message>
+</context>
+</TS>
