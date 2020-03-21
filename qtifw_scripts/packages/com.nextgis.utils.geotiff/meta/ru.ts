100a848a0967750b53c83b136c3acee7eb6e6130
Dmitry Baryshnikov <bishop.dev@gmail.com>
2020-03-21 22:37:16 +0300

    Add geotiff utils

---
+++ qtifw_scripts/packages/com.nextgis.utils.geotiff/meta/ru.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>script</name>
+    <message>
+        <source>GeoTIFF utilities</source>
+        <translation>Утилиты GeoTIFF</translation>
+    </message>
+    <message>
+        <source>GeoTIFF command line utilities</source>
+        <translation>Утилиты командной строки GeoTIFF</translation>
+    </message>
+</context>
+</TS>
