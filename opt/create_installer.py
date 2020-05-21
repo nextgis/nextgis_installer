17f53a4b9f094d64a29dc72da9755e89494d88b8
d3n15 <mushishi.panic@gmail.com>
2020-05-21 10:29:52 +0300

    Added lib_sentrynative

--- opt/create_installer.py
+++ opt/create_installer.py
@@ -57,6 +57,7 @@ repositories = ['lib_z', 'lib_openssl', 'lib_curl', 'lib_sqlite', 'lib_gif',
     'nextgisqgis', 'py_kiwisolver', 'lib_ngstd', 'formbuilder', 'lib_opencv',
     'manuscript', 'lib_oci', 'py_shapely', 'lib_uriparser', 'lib_kml', 'py_proj',
     'lib_bzip2', 'py_pillow', 'lib_harfbuzz', 'lib_littlecms', 'lib_webp',
+    'lib_sentrynative',
 ]
 
 repositories_win = ['lib_iconv', 'python', 'py_sci',
