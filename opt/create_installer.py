c413b13ea2fb0a2dacf3e0d7ef0efdd3d0d4ef03
d3n15 <x514ckw4r3@gmail.com>
2020-07-16 16:56:02 +0300

    Added com.nextgis.common.xslt and com.nextgis.python.lxml

--- opt/create_installer.py
+++ opt/create_installer.py
@@ -59,7 +59,7 @@ repositories = ['lib_z', 'lib_openssl', 'lib_curl', 'lib_sqlite', 'lib_gif',
     'nextgisqgis', 'py_kiwisolver', 'lib_ngstd', 'formbuilder', 'lib_opencv',
     'manuscript', 'lib_oci', 'py_shapely', 'lib_uriparser', 'lib_kml', 'py_proj',
     'lib_bzip2', 'py_pillow', 'lib_harfbuzz', 'lib_littlecms', 'lib_webp',
-    'lib_sentrynative',
+    'lib_sentrynative', 'py_lxml', 'lib_xslt',
 ]
 
 repka_repositories = [
