15afca06b83f580b0c555165835eecdb7becba0b
d3n15 <x514ckw4r3@gmail.com>
2020-12-24 15:56:39 +0300

    Added com.nextgis.utils.utils

---
+++ qtifw_scripts/packages/com.nextgis.utils.utils/meta/ru.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>script</name>
+    <message>
+        <source>NextGIS utilities</source>
+        <translation>Утилиты NextGIS</translation>
+    </message>    
+    <message>
+        <source>Various NextGIS console utilities</source>
+        <translation>Утилиты командной строки NextGIS</translation>
+    </message>
+</context>
+</TS>
