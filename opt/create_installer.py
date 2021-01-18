9320d6832bb321e0fee7bd2e3698387e6b42e3dc
Dmitry Baryshnikov <bishop.dev@gmail.com>
2021-01-18 17:38:10 +0300

    Get webp from repka

--- opt/create_installer.py
+++ opt/create_installer.py
@@ -61,7 +61,7 @@ repositories = ['lib_z', 'lib_openssl', 'lib_gif',
     'lib_spatialindex', 'lib_qwt', 'py_qt4', 
     'nextgisqgis', 'py_kiwisolver', 'lib_ngstd', 'formbuilder', 'lib_opencv',
     'manuscript', 'lib_oci', 'py_shapely', 'lib_uriparser', 'lib_kml', 'py_proj',
-    'lib_bzip2', 'py_pillow', 'lib_harfbuzz', 'lib_littlecms', 'lib_webp',
+    'lib_bzip2', 'py_pillow', 'lib_harfbuzz', 'lib_littlecms', 
     'lib_sentrynative', 'py_lxml', 'lib_xslt', 'nextgis_datastore',
 ]
 
@@ -74,6 +74,7 @@ repka_repositories = [
     {'package': 'lib_sqlite', 'version': 'latest'},
     {'package': 'lib_png', 'version': 'latest'},
     {'package': 'lib_curl', 'version': 'latest'},
+    {'package': 'lib_webp', 'version': 'latest'},
 ]
 
 repositories_win = ['lib_iconv', 'py_sci',
