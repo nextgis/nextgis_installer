813e036a7b0807a3aab087aacb11f7201dfbe025
Dmitry Baryshnikov <bishop.dev@gmail.com>
2021-01-21 12:31:57 +0300

    Update copyright year

--- data/all/run_ng_env.bat
+++ data/all/run_ng_env.bat
@@ -5,4 +5,4 @@ set SSL_CERT_FILE=TargetDir\share\ssl\certs\cert.pem
 set CURL_CA_BUNDLE=TargetDir\share\ssl\certs\cert.pem
 set GDAL_DATA=TargetDir\share\gdal
 @echo on
-@echo. & @echo ************************************************************ & @echo ** Welcome to NextGIS Command Prompt & @echo ** Copyright (C) 2011-2020, NextGIS & @echo ************************************************************ & @echo. & cmd.exe /k
+@echo. & @echo ************************************************************ & @echo ** Welcome to NextGIS Command Prompt & @echo ** Copyright (C) 2011-2021, NextGIS & @echo ************************************************************ & @echo. & cmd.exe /k
