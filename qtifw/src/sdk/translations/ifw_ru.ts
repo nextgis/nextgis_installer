d5dc77e634f25204fe9eaf2dbcc0f83657e29b96
Dmitry Baryshnikov <bishop.dev@gmail.com>
2021-07-12 11:27:37 +0300

    Fix #57. Typo in russian translation

--- qtifw/src/sdk/translations/ifw_ru.ts
+++ qtifw/src/sdk/translations/ifw_ru.ts
@@ -1464,7 +1464,7 @@ Error while loading %2</source>
     </message>
     <message>
         <source>I accept the licenses.</source>
-        <translation>Я согласен(а) с лиценией.</translation>
+        <translation>Я согласен(а) с лицензией.</translation>
     </message>
 </context>
 <context>
