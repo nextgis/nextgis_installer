b5091378ffb3fa6caa55f8761bbfc702a80a4da4
Dmitry Baryshnikov <bishop.dev@gmail.com>
2021-11-16 00:59:16 +0300

    Fix for py3

--- opt/create_installer.py
+++ opt/create_installer.py
@@ -22,6 +22,7 @@ import json
 
 if sys.version_info[0] >= 3:
     from urllib.request import urlretrieve, urlopen
+    unicode = str
 else:
     # Not Python 3 - today, it is most likely to be Python 2
     # But note that this might need an update when Python 4
