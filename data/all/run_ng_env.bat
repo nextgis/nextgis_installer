0f7d3f9af811d44678d08ffe0a949d808bc35e2a
d3n15 <x514ckw4r3@gmail.com>
2022-05-19 15:46:47 +0300

    Added PYTHONPATH to run_ng_env.bat

--- data/all/run_ng_env.bat
+++ data/all/run_ng_env.bat
@@ -4,5 +4,6 @@ set PROJ_LIB=TargetDir\share\proj
 set SSL_CERT_FILE=TargetDir\share\ssl\certs\cert.pem
 set CURL_CA_BUNDLE=TargetDir\share\ssl\certs\cert.pem
 set GDAL_DATA=TargetDir\share\gdal
+set PYTHONPATH=TargetDir\lib\python38\;TargetDir\lib\python38\site-packages;TargetDir\lib\python38\lib-dynload
 @echo on
 @echo. & @echo ************************************************************ & @echo ** Welcome to NextGIS Command Prompt & @echo ** Copyright (C) 2011-2021, NextGIS & @echo ************************************************************ & @echo. & cmd.exe /k
