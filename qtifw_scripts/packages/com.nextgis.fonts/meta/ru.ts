6dbc77d330c5501a9781a12cdb3429db1c3a0d14
Ivan Barsukov <i.barsukov@protonmail.com>
2024-11-28 11:44:18 +0700

    Move fonts

---
+++ qtifw_scripts/packages/com.nextgis.fonts/meta/ru.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>script</name>
+    <message>
+        <source>Fonts</source>
+        <translation>Шрифты</translation>
+    </message>
+    <message>
+        <source>Additional fonts</source>
+        <translation>Дополнительные шрифты</translation>
+    </message>
+</context>
+</TS>
